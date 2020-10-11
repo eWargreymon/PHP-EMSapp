75451ddd56d2c6a8cc4e825d1e6ba0c387d4b834
eWargreymon <eddyhb1@hotmail.com>
2020-10-11 18:20:19 +0100

    Login and Register form + css

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Inicio</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-4 col-lg-push-4 col-md-push-4">
+                <div class="card bg-warning" style="margin-top: 50px;">
+                    <div class="card-title pt-2 pl-3 cardTitle">Login</div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="form-group mb-3">
+                                <input type="text" class="form-control input-sm" name="uusername" required placeholder="Username">
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="password" class="form-control input-sm" name="upass" required placeholder="Password">
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="submit" class="btn btn-success btn-sm" name="ulogin" value="Login">
+                                <a href="register.php" class="btn btn-info btn-sm registerLink">Register</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+
+    <title>Inicio</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-4 col-lg-push-4 col-md-push-4">
+                <div class="card bg-danger" style="margin-top: 50px;">
+                    <div class="card-title pt-2 pl-3 cardTitle">Register</div>
+                    <p class="pl-3 mb-0 cardTitleP">Add de the data required</p>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="form-group mb-3">
+                                <input type="email" class="form-control input-sm" name="uemail" required placeholder="Email">
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="text" class="form-control input-sm" name="uusername" required placeholder="Username">
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="password" class="form-control input-sm" name="upass" required placeholder="Password">
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="submit" class="btn btn-success btn-sm" name="uregister" value="Register">
+                                <a href="index.php" class="btn btn-outline btn-sm backLink">Back</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
